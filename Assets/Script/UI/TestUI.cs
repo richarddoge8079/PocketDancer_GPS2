dba406746f13439657497e14dc75ace1580c4741
Jimmyboyz <0111370@kdu-online.com>
2016-10-04 11:35:52 +0800

    Detection Meter Bar
    In this file, i have input the Detection Slider UI script inside the file.

--- Assets/Script/UI/TestUI.cs
+++ Assets/Script/UI/TestUI.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -6,6 +6,7 @@ public class TestUI : MonoBehaviour {
 
 	public Text moneyText;
 	public Text detectionText;
+	public Slider DetectionBar;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +17,6 @@ public class TestUI : MonoBehaviour {
 	void Update () {
 		moneyText.text = "Money : " + GameManager.Instance.playerStatsScript.moneyCount;
 		detectionText.text = "Detection Level : " + GameManager.Instance.playerStatsScript.detectionLevel + "%";
+		DetectionBar.value = GameManager.Instance.playerStatsScript.detectionLevel;
 	}
 }
