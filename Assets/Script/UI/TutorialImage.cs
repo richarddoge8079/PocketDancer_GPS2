53635320cd5b03a4703979b36c1a4291165aeab5
Hoh Yu Onn <hohyuonn95@gmail.com>
2016-10-05 02:01:46 +0800

    Weird Raycast

--- Assets/Script/UI/TutorialImage.cs
+++ Assets/Script/UI/TutorialImage.cs
@@ -4,36 +4,34 @@ using UnityEngine.UI;
 
 public class TutorialImage : MonoBehaviour {
 
+	public float tutorialRange;
+	RaycastHit isTutorial;
 	public Image tutImage;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		Ray tutorialRay = new Ray (transform.localPosition, transform.right);
+		if (Physics.Raycast (tutorialRay, out isTutorial, tutorialRange)) {
+			checkPlayer ();
+		} else {
+			Debug.Log ("kdone");
 
-	void OnTriggerEnter(Collider coll){
-		if(coll.CompareTag("Player")){
-			UIManager.Instance.EnableTutorial(true);
-			UIManager.Instance.tutorialImage.sprite = tutImage.sprite;
 		}
+		Debug.DrawRay (transform.localPosition, transform.right * tutorialRange, Color.red);
 	}
-	void OnTriggerExit(Collider coll){
-		if(coll.CompareTag("Player")){
-//						Debug.Log ("bb");
-			//			UIManager.Instance.tutorialImage = tutImage;
-//			Color color = UIManager.Instance.tutorialImageColor;
-//			color.a = 1.0f;
-//
-//			UIManager.Instance.tutorialImageColor = color;
-//			UIManager.Instance.tutorialImageColor.a = 0.0f;
 
-			UIManager.Instance.EnableTutorial(false);
-
+	void checkPlayer()
+	{
+		if (isTutorial.collider.CompareTag ("Player")) {
+			UIManager.Instance.EnableTutorial (true);	
+			UIManager.Instance.tutorialImage.sprite = tutImage.sprite;
+		} else {
+			UIManager.Instance.EnableTutorial (false);
 		}
 	}
 }
