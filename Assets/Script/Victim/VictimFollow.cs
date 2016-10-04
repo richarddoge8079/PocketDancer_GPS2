ca52c84d73c268e682e3c0088bc0832e3c267308
FelinxRynn <felinxrynn@gmail.com>
2016-10-04 22:26:03 +0800

    VictimFollower

---
+++ Assets/Script/Victim/VictimFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictimFollow : MonoBehaviour {
+
+
+	public float smoothTimerX,smoothTimerY,smoothTimerZ;
+
+	public float xSpeed,ySpeed,zSpeed; //xDistance,yDistance,zDistance;
+
+	public GameObject victimObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float posX = Mathf.SmoothDamp (transform.position.x, victimObject.transform.position.x, ref xSpeed,smoothTimerX);
+		float posY = Mathf.SmoothDamp (transform.position.y, victimObject.transform.position.y, ref ySpeed,smoothTimerY);
+		float posZ = Mathf.SmoothDamp (transform.position.z, victimObject.transform.position.z, ref zSpeed,smoothTimerZ);
+
+		transform.position = new Vector3 (posX,posY,posZ);
+
+		transform.rotation = victimObject.transform.rotation;
+	}
+
+	public void SetFollow(GameObject ga){
+		victimObject = ga;
+	}
+}
---
+++ Assets/Script/Victim/VictimFollowerSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictimFollowerSpawner : MonoBehaviour {
+
+	public GameObject victimFollower;
+	public GameObject ga;
+
+	// Use this for initialization
+	void Start () {
+//		ga = Instantiate (victimFollower,transform.position,Quaternion.identity);
+		ga = Instantiate (victimFollower);
+		ga.SendMessage ("SetFollow", this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
