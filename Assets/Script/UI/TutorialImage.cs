1a1fb73a32289b520f1266a16cfc497d085e913c
Hoh Yu Onn <hohyuonn95@gmail.com>
2016-10-06 00:21:36 +0800

    Tutorial works fine now

--- Assets/Script/UI/TutorialImage.cs
+++ Assets/Script/UI/TutorialImage.cs
@@ -3,35 +3,41 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class TutorialImage : MonoBehaviour {
-
+	public GameObject tutorialndicator;
+	public GameObject tutorialndicator2;
+	public float timer;
 	public float tutorialRange;
-	RaycastHit isTutorial;
+	RaycastHit hit;
 	public Image tutImage;
+	public bool displayed;
 
 	// Use this for initialization
 	void Start () {
-
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Ray tutorialRay = new Ray (transform.localPosition, transform.right);
-		if (Physics.Raycast (tutorialRay, out isTutorial, tutorialRange)) {
+		Ray tutorialRay = new Ray (transform.position, transform.right);
+		if (Physics.Raycast (tutorialRay, out hit, tutorialRange)) {
 			checkPlayer ();
-		} else {
-//			Debug.Log ("kdone");
-
-		}
-		Debug.DrawRay (transform.localPosition, transform.right * tutorialRange, Color.red);
+		} 
+		Debug.DrawRay (transform.position, transform.right * tutorialRange, Color.red);
 	}
 
 	void checkPlayer()
 	{
-		if (isTutorial.collider.CompareTag ("Player")) {
-			UIManager.Instance.EnableTutorial (true);	
+		if (hit.collider.CompareTag ("Player")) {
 			UIManager.Instance.tutorialImage.sprite = tutImage.sprite;
-		} else {
-			UIManager.Instance.EnableTutorial (false);
-		}
+			UIManager.Instance.EnableTutorial (true);
+			StartCoroutine (EndDisplay (timer));
+		} 
+	}
+
+	IEnumerator EndDisplay(float delay)
+	{
+		yield return new WaitForSeconds (delay);
+		UIManager.Instance.EnableTutorial (false);
+		tutorialndicator.SetActive(false);
+		tutorialndicator2.SetActive(false);
 	}
 }
