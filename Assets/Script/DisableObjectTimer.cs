ddc5d67609841fb57e6d3e5109f7c46659c0d221
FelinxRynn <felinxrynn@gmail.com>
2016-10-13 11:37:50 +0800

    SomeStuffs

---
+++ Assets/Script/DisableObjectTimer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableObjectTimer : MonoBehaviour {
+
+	public float timer;
+
+	void OnEnable(){
+		StopCoroutine ("DisableTimer");
+		StartCoroutine ("DisableTimer",timer);
+	}
+
+	IEnumerator DisableTimer(float t){
+		yield return new WaitForSeconds (t);
+		gameObject.SetActive (false);
+	}
+}
