e3d647db1d5b3c267ece58b2473575453d893fe6
Richard Lee Guan Rong <guanrong1226@gmail.com>
2016-10-20 00:28:12 +0800

    Stage 1 of Upgrade System because fuck my mind

---
+++ Assets/Script/Player/PlayerUpgrade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUpgrade : MonoBehaviour {
+
+	int Money;
+	public bool upgrade1Active;
+	public bool upgrade2Active;
+	public bool upgrade3Active;
+
+	// Use this for initialization
+	void Start () {
+		Money = gameObject.GetComponent<PlayerStats> ().moneyCount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
