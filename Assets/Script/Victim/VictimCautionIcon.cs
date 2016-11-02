5c25eee90098858ea5ea71c562910108f123aba3
Jimmyboyz <0111370@kdu-online.com>
2016-11-03 01:05:01 +0800

    Victim Pop Up Icon is uploaded

---
+++ Assets/Script/Victim/VictimCautionIcon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictimCautionIcon : MonoBehaviour 
+{
+	public GameObject questionMark;
+	public GameObject exclamationMark;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GameManager.Instance.inSight == true && GameManager.Instance.playerStatsScript.detectionLevel > 0.0f) 
+		{
+			questionMark.SetActive (true);
+		} 
+		else if (GameManager.Instance.inSight == true && GameManager.Instance.playerStatsScript.detectionLevel == 100.0f) 
+		{
+			exclamationMark.SetActive (true);
+		} 
+	}
+}
