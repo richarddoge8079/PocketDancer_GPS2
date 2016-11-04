867d0959668a4006c59ecb21ca48b1ae9e183491
ASUS-PC\Zer0 <xaberix12@yahoo.com>
2016-11-05 00:55:33 +0800

    Victim Collision

--- Assets/Script/Victim/VictimCollision.cs
+++ Assets/Script/Victim/VictimCollision.cs
@@ -10,10 +10,13 @@ public class VictimCollision : MonoBehaviour {
 	public GameObject victimBackFX;
 
 	public float pickpocketRange;
+	public bool playerInRangeFront = false;
 	public bool playerInRangeBack = false;
 	public bool playerInRangeLeft = false;
 	public bool playerInRangeRight = false;
 	public bool picked = false;
+	public int maxFrontMoney;
+	public int minFrontMoney;
 	public int maxBackMoney;
 	public int minBackMoney;
 	public int maxLeftMoney;
@@ -35,6 +38,7 @@ public class VictimCollision : MonoBehaviour {
 	void Update ()
 	{
 		Ray pickpocketRayBack = new Ray (transform.localPosition, -transform.forward);
+		Ray pickpocketRayFront = new Ray (transform.localPosition, transform.forward);
 		Ray pickpocketRayRight = new Ray (transform.localPosition, transform.right);
 		Ray pickpocketRayLeft = new Ray (transform.localPosition, -transform.right);
 		if (Physics.Raycast (pickpocketRayBack, out isPickpocketed, pickpocketRange)) 
@@ -58,6 +62,14 @@ public class VictimCollision : MonoBehaviour {
 			if (isPickpocketed.collider.CompareTag("Player")) 
 			{
 				playerInRangeLeft = true;
+				//Debug.Log ("Target In Range");
+			} 
+		} 
+		else if (Physics.Raycast (pickpocketRayFront, out isPickpocketed, pickpocketRange)) 
+		{
+			if (isPickpocketed.collider.CompareTag("Player")) 
+			{
+				playerInRangeFront = true;
 				//Debug.Log ("Target In Range");
 			} 
 		} 
@@ -207,12 +219,57 @@ public class VictimCollision : MonoBehaviour {
 				GameManager.Instance.playerStatsScript.detectionLevel += detectionLevel;
 			}
 		}
+		if (playerInRangeFront)  
+		{
+			if (!picked)
+			{
+				//					Debug.Log ("I've just been robbed!?");
+				//Instantiate(rob, GameManager.Instance.playerObject.transform.position + new Vector3(0f, 1.5f, 0f), Quaternion.identity);
+
+				GameObject go = ObjectPoolingScript.Instance.GetObject ("MoneyParticleSystem");
+
+				if (go != null) 
+				{
+					//! Reseting the bullet attributes
+					go.transform.position = transform.position;
+				}
+
+				picked = true;
+
+				//					Debug.Log ("I've just been robbed!?");
+				money = Random.Range (minFrontMoney, maxFrontMoney);
+				if (UIManager.Instance.updateTotalMoney) 
+				{
+					UIManager.Instance.UiVictimMoney += money;
+					GameManager.Instance.pickPocket += 1;
+
+					picked = true;
+				}
+				else
+				{
+					UIManager.Instance.UiVictimMoney += money;
+					UIManager.Instance.UpdateMoney ();
+
+					picked = true;
+				}
+			} 
+			else 
+			{
+				//					Debug.Log ("Why did someone touch my butt?!");
+				GameManager.Instance.playerStatsScript.detectionLevel += detectionLevel;
+				if (GameManager.Instance.playerStatsScript.detectionLevel >= 50) 
+				{
+					//						Instantiate(questionMark, transform.localPosition + new Vector3(0f, 1.5f, 0f), )
+				}
+			}
+		} 
 	}
 
 	IEnumerator Timer(float Delay)
 	{
 		yield return new WaitForSeconds (Delay);
 		playerInRangeBack= false;
+		playerInRangeFront= false;
 		playerInRangeRight = false;
 		playerInRangeLeft = false;
 	}
