ff232bbda774c600ef702bc6ed2bf773a02aa3cc
danielledeluca <0110460@kdu-online.com>
2016-11-10 03:26:32 +0800

    Add files via upload

--- Assets/Script/UI/FloatingMoney.cs
+++ Assets/Script/UI/FloatingMoney.cs
@@ -5,19 +5,32 @@ using UnityEngine.UI;
 public class FloatingMoney : MonoBehaviour {
 	public bool move;
 	// Use this for initialization
-	void Start () 
+//	void Start()
+//	{
+//		StopCoroutine (Timer(1.0f));
+//		StartCoroutine (Timer (1.0f));
+//	}
+	void OnEnable()
 	{
-		move = true;	
+		StopCoroutine (Timer(0.5f));
+		StartCoroutine (Timer (0.5f));
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
-		if (move) {
-			gameObject.GetComponent<RectTransform> ().localPosition = Vector3.Lerp (gameObject.GetComponent<RectTransform> ().localPosition, GameObject.FindGameObjectWithTag ("Wallet").GetComponent<RectTransform> ().localPosition, 0.5f);
+		if (move) 
+		{
+			gameObject.GetComponent<RectTransform> ().localPosition = Vector2.Lerp (gameObject.GetComponent<RectTransform> ().localPosition, GameObject.FindGameObjectWithTag ("Wallet").GetComponent<RectTransform> ().localPosition, 0.5f);
 			if (gameObject.GetComponent<RectTransform> ().localPosition == GameObject.FindGameObjectWithTag ("Wallet").GetComponent<RectTransform> ().localPosition) {
 				Destroy (gameObject);
 			}
-		} 
+		}
 	}
+	IEnumerator Timer (float Delay)
+	{
+		yield return new WaitForSeconds (Delay);
+		move = true;
+	}
+
 }
