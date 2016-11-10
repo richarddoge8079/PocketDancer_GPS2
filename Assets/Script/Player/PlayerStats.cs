1b67a066c3a87bee9766c382acac02575ffe557d
Jimmyboyz <0111370@kdu-online.com>
2016-11-10 09:40:33 +0800

    Upgrade no. 6 is implemented

--- Assets/Script/Player/PlayerStats.cs
+++ Assets/Script/Player/PlayerStats.cs
@@ -32,15 +32,34 @@ public class PlayerStats : MonoBehaviour {
 //				GameManager.Instance.RestartLevel ();
 //			}
 		} 
-		else {
-			if (detectionLevel <= 99.0f) {
+		else 
+		{
+			if (DataManager.Instance.upgrade6Active == false) 
+			{
+				if(detectionLevel <= 99.0f)
+				{
 				detectionLevel += 20.0f * Time.deltaTime;
-			} 
-			else {
-				detectionLevel = 100.0f;
+				} 
+				else
+				{
+					detectionLevel = 100.0f;
+				}
+			}
+
+			else 
+			{
+				if(detectionLevel <= 104.0f)
+				{
+					detectionLevel += 20.0f * Time.deltaTime;
+				} 
+				else
+				{
+					detectionLevel = 105.0f;
+				}
 			}
 		}
-		if(detectionLevel >= 100.0f){
+		if(DataManager.Instance.upgrade6Active == false && detectionLevel >= 100.0f || DataManager.Instance.upgrade6Active == true && detectionLevel >= 105.0f)
+		{
 			isDetected = true;
 		}
 	}
