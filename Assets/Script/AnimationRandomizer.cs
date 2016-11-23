ae3fe21b76b201403a2c24eabaca4019e3261855
ASUS-PC\Zer0 <xaberix12@yahoo.com>
2016-11-23 16:14:02 +0800

    AnimationRandomizer

---
+++ Assets/Script/AnimationRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationRandomizer : MonoBehaviour {
+
+	public Animation animationReference;
+	public AnimationClip[] clips;
+	int _currClip = 0;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown ("1")) 
+		{
+			OnCompleteOfClip ();
+		}
+	}
+	void OnCompleteOfClip()
+	{
+		_currClip = Random.Range(0,2);
+		Debug.Log (_currClip);
+		if(_currClip == clips.Length) return;
+		{
+			animationReference.clip = clips [_currClip];
+//		Debug.Log (_currClip);
+			animationReference.Play ();
+		}
+	}
+}
