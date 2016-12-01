4a8f99f5f289382bb6605c3924d8c750b3932c72
ASUS-PC\Zer0 <xaberix12@yahoo.com>
2016-12-02 03:18:48 +0800

    Vibration System YEAY!!!!

---
+++ Assets/Script/VibrationScripts/StrongVibration.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrongVibration : MonoBehaviour {
+
+	public bool canVibrate;
+	public float vibrationalTimer;
+	// Use this for initialization
+	void Start () 
+	{
+		canVibrate = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (canVibrate) 
+		{
+			Debug.Log ("Vibrating");
+			//			Timer (vibratio'nalTimer);
+			StartCoroutine("Timer", vibrationalTimer);
+		} 
+
+	}
+	public void VibrateFunction ()
+	{
+		canVibrate = true;
+	}
+
+	IEnumerator Timer(float t)
+	{
+		yield return new WaitForSeconds (t);
+		Handheld.Vibrate ();
+		yield return new WaitForSeconds (t);
+		Handheld.Vibrate ();
+		yield return new WaitForSeconds (t);
+		Handheld.Vibrate ();
+		canVibrate = false;
+	}
+}
